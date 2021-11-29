2a910255ee4a0cfbbc00cabd7ab0b118d23458b0
saltrupiao <strupiano@oakland.edu>
2021-11-29 17:36:10 -0500

    Code cleanup for Vehicle If Case

--- matchResults.php
+++ matchResults.php
@@ -22,7 +22,7 @@
                         <a class="nav-link" href="about.html">About CME</a>
                     </div>
                 </div>
-            </nav>';
+            </nav><br><div class="container">';
 
 
     session_start();
@@ -60,6 +60,7 @@
             }
         }
 
+        /* Echo Debug Statements for Customer ID and Customer Domain
         echo "<h1>Get Customer ID: ";
         echo $custID;
         echo "</h1>";
@@ -67,6 +68,7 @@
         echo "<h1>Customer Domain: ";
         echo $custDomain;
         echo "</h1>";
+       */
 
         if ($custDomain == 'Vehicles') {
 
@@ -76,74 +78,79 @@
             $cpBodyStyle = $_POST['BodyStyle'];
             //Get Multiplyer
             $cpBodyStylePriority = $_POST['BodyStylePriority'];
-
+            /* Debug Statements
             echo "<h1>Body Style ID: $cpBodyStyleID</h1>";
             echo "<h1>Body Style: $cpBodyStyle</h1>";
             echo "<h1>Priority: $cpBodyStylePriority</h1><br>";
+            */
 
             $cpColorID = $_POST['ColorID'];
             $cpColor = $_POST['Color'];
             $cpColorPriority = $_POST['ColorPriority'];
-
+            /* Debug Statements
             echo "<h1>Color ID: $cpColorID</h1>";
             echo "<h1>Color: $cpColor</h1>";
             echo "<h1>Priority: $cpColorPriority</h1><br>";
+            */
 
             $cpEngineID = $_POST['EngineID'];
             $cpEngine = $_POST['Engine'];
             $cpEnginePriority = $_POST['EnginePriority'];
-
+            /* Debug Statements
             echo "<h1>Engine ID: $cpEngineID</h1>";
             echo "<h1>Engine: $cpEngine </h1>";
             echo "<h1>Priority: $cpEnginePriority</h1><br>";
+            */
 
             $cpTransmissionID = $_POST['TransmissionID'];
             $cpTransmission = $_POST['Transmission'];
             $cpTransmissionPriority = $_POST["TransmissionPriority"];
-
+            /* Debug Statements
             echo "<h1>Trans ID: $cpTransmissionID</h1>";
             echo "<h1>Trans: $cpTransmission </h1>";
             echo "<h1>Priority: $cpTransmissionPriority</h1><br>";
+            */
 
             $cpDriveID = $_POST['DriveID'];
             $cpDrive = $_POST['Drive'];
             $cpDrivePriority = $_POST["DrivePriority"];
-
+            /* Debug Statements
             echo "<h1>Drive ID: $cpDriveID</h1>";
             echo "<h1>Drive: $cpDrive</h1>";
             echo "<h1>Priority: $cpDrivePriority</h1><br>";
+            */
 
             $sqlInsertCustProfileBody = "INSERT INTO CustProfile (CustomerID, DomainID, FeatureID, DesiredFeatureValue, DesiredFeatureValueName, Multiplier) VALUES ($custID, $custDomainID, $cpBodyStyleID, NULL, '$cpBodyStyle', $cpBodyStylePriority)";
             if ($conn->query($sqlInsertCustProfileBody) === TRUE) {
-                echo "Body Insert Successful";
+                //echo "Body Insert Successful";
             } else {
                 echo "error: " . $sqlInsertCustProfileBody . "<br>" . $conn->error;
             }
 
             $sqlInsertCustProfileColor = "INSERT INTO CustProfile (CustomerID, DomainID, FeatureID, DesiredFeatureValue, DesiredFeatureValueName, Multiplier) VALUES ($custID, $custDomainID, $cpColorID, NULL, '$cpColor', $cpColorPriority)";
             if ($conn->query($sqlInsertCustProfileColor) === TRUE) {
-                echo "Color Insert Successful";
+                //echo "Color Insert Successful";
             } else {
                 echo "error: " . $sqlInsertCustProfileColor . "<br>" . $conn->error;
             }
 
             $sqlInsertCustProfileEngine = "INSERT INTO CustProfile (CustomerID, DomainID, FeatureID, DesiredFeatureValue, DesiredFeatureValueName, Multiplier) VALUES ($custID, $custDomainID, $cpEngineID, NULL, '$cpEngine', $cpEnginePriority)";
             if ($conn->query($sqlInsertCustProfileEngine) === TRUE) {
-                echo "Engine Insert Successful";
+                //echo "Engine Insert Successful";
             } else {
                 echo "error: " . $sqlInsertCustProfileEngine . "<br>" . $conn->error;
             }
 
             $sqlInsertCustProfileTrans = "INSERT INTO CustProfile (CustomerID, DomainID, FeatureID, DesiredFeatureValue, DesiredFeatureValueName, Multiplier) VALUES ($custID, $custDomainID, $cpTransmissionID, NULL, '$cpTransmission', $cpTransmissionPriority)";
             if ($conn->query($sqlInsertCustProfileTrans) === TRUE) {
-                echo "Trans Insert Successful";
+                //echo "Trans Insert Successful";
             } else {
                 echo "error: " . $sqlInsertCustProfileTrans . "<br>" . $conn->error;
             }
 
             $sqlInsertCustProfileDrive = "INSERT INTO CustProfile (CustomerID, DomainID, FeatureID, DesiredFeatureValue, DesiredFeatureValueName, Multiplier) VALUES ($custID, $custDomainID, $cpDriveID, NULL, '$cpDrive', $cpDrivePriority)";
             if ($conn->query($sqlInsertCustProfileDrive) === TRUE) {
-                echo "Trans Insert Successful";
+                //echo "Trans Insert Successful";
             } else {
                 echo "error: " . $sqlInsertCustProfileDrive . "<br>" . $conn->error;
             }
@@ -152,7 +159,7 @@
             //Drop StockPoints Table -- Reference: https://www.w3schools.com/php/php_mysql_delete.asp
             $sqlDropStockPoints = "DROP TABLE IF EXISTS StockPoints";
             if ($conn->query($sqlDropStockPoints) === TRUE) {
-                echo "<h2>StockPoints Deleted Successfully</h2>";
+                //echo "<h2>StockPoints Deleted Successfully</h2>";
             } else {
                 echo "Error Deleting Record: " . $conn->error;
             }
@@ -166,14 +173,14 @@
                                         S.DomainID = '$custDomainID' and CustomerID = '$custID') order by stocknum, featureID";
 
             if ($conn->query($sqlCreateStockPoints) === TRUE) {
-                echo "<h2> Create Stock Points Success! </h2>";
+                //echo "<h2> Create Stock Points Success! </h2>";
             } else {
                 echo "Error Creating Stock Points Record: " . $conn->error;
             }
 
             $sqlDropStockMatch = "DROP TABLE IF EXISTS StockMatch";
             if ($conn->query($sqlDropStockMatch) === TRUE) {
-                echo "<h2>StockMatch Deleted Successfully</h2>";
+                //echo "<h2>StockMatch Deleted Successfully</h2>";
             } else {
                 echo "Error Deleting Record: " . $conn->error;
             }
@@ -184,21 +191,21 @@
                                     Group by StockNum
                                     order by TotalPoints DESC";
             if ($conn->query($sqlCreateStockMatch) === TRUE) {
-                echo "<h2> Create Stock Match Success! </h2>";
+                //echo "<h2> Create Stock Match Success! </h2>";
             } else {
                 echo "Error Creating Stock Match Record: " . $conn->error;
             }
 
             $sqlDeleteCustMatch = "DELETE FROM CustMatch WHERE DomainID = '$custDomainID' AND CustomerID = '$custID'";
             if ($conn->query($sqlDeleteCustMatch) === TRUE) {
-                echo "<h2>CustMatch Deleted For Existing Customer Successfully</h2>";
+                //echo "<h2>CustMatch Deleted For Existing Customer Successfully</h2>";
             } else {
                 echo "Error Deleting Record: " . $conn->error;
             }
 
             $sqlInsertUpdatedCustomerMatch = "INSERT INTO CustMatch SELECT CustomerID, DomainID, StockNum, StockDesc, TotalPoints FROM StockMatch";
             if ($conn->query($sqlInsertUpdatedCustomerMatch) === TRUE) {
-                echo "<h2> Insert Customer Match FINAL Query Success! </h2>";
+                //echo "<h2> Insert Customer Match FINAL Query Success! </h2>";
             } else {
                 echo "Error Inserting into Customer Match Record: " . $conn->error;
             }
